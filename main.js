9f310d35dce5ba9951997e1b3053676977061b09
Dmitrii Marov <dimamarov@gmail.com>
2025-12-25 14:51:18 +0300

    Update main.js
    Считаем только фреймы самого верхнего уровня, исключаем вложенные.

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-async function countTopLevelFrames() {
-  const topLevelFrames = pixso.currentPage.children.filter(
-    node => node.type === 'FRAME' && node.visible !== false
+function countVisibleTopLevelFramesInFile() {
+  const pages = pixso.root.children.filter(node => node.type === 'PAGE');
+
+  const visibleTopLevelFrames = pages.flatMap(page =>
+    page.children.filter(child => child.type === 'FRAME' && child.visible !== false)
   );
 
-  pixso.notify(`Видимых верхнеуровневых фреймов: ${topLevelFrames.length}`);
+  pixso.notify(`Видимых фреймов во всём файле: ${visibleTopLevelFrames.length}`);
 }
 
-countTopLevelFrames();
+countVisibleTopLevelFramesInFile();
